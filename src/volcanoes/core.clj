f1ab63ccc76a465c47c959c5d854ef1f3e6262e0
kraulain <kraulain@gmail.com>
2023-05-16 14:41:33 +0100

    Add comment for csv-lines symbol

--- src/volcanoes/core.clj
+++ src/volcanoes/core.clj
@@ -2,7 +2,8 @@
   (:require [clojure.data.csv :as csv]
            [clojure.java.io :as io]))
 
-(def csv-lines
+;; Read csv file and save in memory
+(def csv-lines 
   (with-open [csv (io/reader "/Users/kraulain/projects/personal/clojure/b2/volcanoes/resources/GVP_Volcano_List_Holocene.csv")]
     (doall
      (csv/read-csv csv))))
