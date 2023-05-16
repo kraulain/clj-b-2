a538e52727a84766b0f9244e5cd3df8e6ef01071
kraulain <kraulain@gmail.com>
2023-05-16 16:32:19 +0100

    Use transform functions to parse volcano records keys as keywords

--- src/volcanoes/core.clj
+++ src/volcanoes/core.clj
@@ -2,18 +2,38 @@
   (:require [clojure.data.csv :as csv]
            [clojure.java.io :as io]))
 
+
 ;; Read csv file and save in memory
-(def csv-lines 
+(def csv-lines
   (with-open [csv (io/reader "/Users/kraulain/projects/personal/clojure/b2/volcanoes/resources/GVP_Volcano_List_Holocene.csv")]
     (doall
      (csv/read-csv csv))))
 
 
+(defn transform-header
+  "Transform a header string into a clojure keyword"
+  [header]
+  (if (= header "Elevation (m)")
+    :elevation-meters
+    (-> header
+        clojure.string/lower-case
+        (clojure.string/replace #" " "-")
+        keyword)))
+
+
+(defn transform-header-row
+  "Transforms the strings in a header row into valid keywords"
+  [header-row]
+  (map transform-header header-row))
+
+
 ;;Extract volcano records into a clojure map
 (def volcano-records
   (let [csv-lines (rest csv-lines)
-        header-line (first csv-lines)
+        header-line (transform-header-row (first csv-lines))
         volcano-lines (rest csv-lines)]
     (map (fn [volcano-line]
            (zipmap header-line volcano-line))
          volcano-lines)))
+
+
