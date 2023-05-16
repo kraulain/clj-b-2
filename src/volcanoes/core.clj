72159bde84815136e7bc44c1f3c2703a9febb512
kraulain <kraulain@gmail.com>
2023-05-16 14:39:22 +0100

    Add function to read in csv file contents

--- src/volcanoes/core.clj
+++ src/volcanoes/core.clj
@@ -1,6 +1,10 @@
-(ns volcanoes.core)
+(ns volcanoes.core
+  (:require [clojure.data.csv :as csv]
+           [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def csv-lines
+  (with-open [csv (io/reader "/Users/kraulain/projects/personal/clojure/b2/volcanoes/resources/GVP_Volcano_List_Holocene.csv")]
+    (doall
+     (csv/read-csv csv))))
+
+
